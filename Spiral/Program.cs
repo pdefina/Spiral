e6743b8c70fe8aa725001501e02e7583f60426bb
Paul Defina <pdefina1@outlook.com>
2020-06-16 01:30:12 -0400

    Add project files.

---
+++ Spiral/Program.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Spiral
+{
+
+    public static class Spiral
+    {        /// <summary>
+             /// Takes a Two Dimensional Array and does a Clockwise spiral on it
+             /// </summary>
+             /// <param name="TDArray">Must be a Two Dimensional Array</param>
+
+             /// <summary>
+             /// Takes a Two Dimensional Array and does a Clockwise spiral on it
+             /// </summary>
+             /// <param name="TDArray">Must be a Two Dimensional Array</param>
+             /// <returns> List<int> </returns>
+        public static List<int> SpiralIt(int[,] TDArray)
+        {
+            int rsi = 0; // Row Start Index 
+            int csi = 1; // Column Start Index
+            int rei = TDArray.GetLength(0); // Row End Index
+            int cei = TDArray.GetLength(1); // Column End Index
+            int i = rsi;
+            List<int> outList = new List<int>();
+
+            while (rsi < rei && csi < cei)
+            {
+                // left to right
+                for (i = rsi; i < rei; i++)
+                {
+                    outList.Add(TDArray[rsi, i]);
+                }
+                rsi++;
+
+                // Down
+                for (i = csi; i < cei; i++)
+                {
+                    outList.Add(TDArray[i, cei - 1]);
+                }
+                cei--;
+
+                // Right to left
+                for (i = rei - 2; i >= rsi - 1; i--)
+                {
+                    outList.Add(TDArray[rei - 1, i]);
+                }
+                rei--;
+
+                // Up
+                for (i = cei - 1; i >= csi; i--)
+                {
+                    outList.Add(TDArray[i, csi - 1]);
+                }
+                csi++;
+            }
+            return outList;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Made the data a little more interesting
+            int[,] array = new int[,]   { { 1, 2, 3, 4  },
+                                          { 5, 6, 7, 8  },
+                                          { 9, 10,11,12 },
+                                          {13, 14,15,16 } };
+            string expectedData = "1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10";
+
+            List<int> mySpiralList = Spiral.SpiralIt(array);
+
+            // Convert to a string for easy printing and testing
+            string actualData = string.Join(" ", mySpiralList.ToArray());
+
+            Console.WriteLine(actualData);
+
+            // Verify
+            Assert.AreEqual(expectedData, actualData);
+
+        }
+    }
+}
